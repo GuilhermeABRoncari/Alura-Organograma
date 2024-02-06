5830b83e3e7e913b34ee048294c5afd248d49e6e
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-02-05 21:56:44 -0300

    Implementa rodapé

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -3,22 +3,23 @@ import './Time.css';
 
 const Time = (props) => {
 
-    const css = {
-        backgroundColor: props.corSecundaria
-    }
+    const css = {backgroundColor: props.corSecundaria}
 
     return (
-        <section className='time' style={css}>
+        (props.colaboradores.lenght > 0) ? <section className='time' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className="colaboradores">
+            <div className='colaboradores'>
                 {props.colaboradores.map(
                     colaborador => <Colaborador 
+                                        corPrimaria={props.corPrimaria}
+                                        key={colaborador.nome}
                                         nome={colaborador.nome}
                                         cargo={colaborador.cargo}
                                         imagem={colaborador.imagem}
                                     />)}
             </div>
         </section>
+        : ''
     );
 }
 
